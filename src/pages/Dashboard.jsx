c7988f9ab5f86c240fca12c0cbe3f39997b836ce
Bernard Almeida da Costa <biernard@gmail.com>
2024-11-27 14:28:54 +0100

    added new icons to the sidebar

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,3 +1,8 @@
+import { FaTasks } from "react-icons/fa"
+import { RiTeamFill } from "react-icons/ri"
+import { IoMdSettings } from "react-icons/io"
+import { GrProjects } from "react-icons/gr"
+
 const Dashboard = () => {
   return (
     <section
@@ -6,11 +11,33 @@ const Dashboard = () => {
       style={{ backgroundImage: "url(https://picsum.photos/1280/900)" }}
     >
       <div className="container p-10 h-screen mt-7 flex gap-5">
-        <div className="lefside w-[25%] bg-white opacity-85 border rounded-xl mr-8 p-4">
-          <h1>SIDEBAR</h1>
-          <div></div>
-          <div></div>
-          <div></div>
+        <div className="lefside w-[25%] bg-white opacity-85 border rounded-xl mr-8 p-4 flex flex-col gap-5 place-items-center place-content-between">
+          <div>
+            <h1>LOGO</h1>
+          </div>
+          <div className="flex flex-col justify-between items-center gap-5">
+            <a className="flex justify-center items-center gap-3" href="#">
+              <GrProjects />
+              Projects
+            </a>
+
+            <a className="flex justify-center items-center gap-3" href="#">
+              <FaTasks />
+              Tasks
+            </a>
+
+            <a className="flex justify-center items-center gap-3" href="#">
+              <RiTeamFill />
+              Team
+            </a>
+            <a className="flex justify-center items-center gap-3" href="#">
+              <IoMdSettings />
+              Settings
+            </a>
+          </div>
+          <div>
+            <h1>INFOS</h1>
+          </div>
         </div>
         <div className="rightside w-[75%] bg-white opacity-95 border rounded-xl p-4 flex flex-col gap-6">
           <div className="top border ">
