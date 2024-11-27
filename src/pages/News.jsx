36d2c203aa463ae98ba06e46008d559dd8941c35
René Weiberlenn <rene.weiberlenn@live.de>
2024-11-27 19:53:00 +0100

    Adjustment of mockdata to adhere to backend

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,11 +1,21 @@
 const News = () => {
+  const dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
   const news = {
     news_id: 1,
     creator_id: 1,
     news_title: "From plastic bottles to boxed water, How can you contribute?",
     news_content: "Lorem Ipsum",
     news_image: '/news/boxed-water-is-better-9qDtdcQoTw8-unsplash.jpg',
-    createdAt: new Date().toISOString(),
+    createdAt: new Date().toLocaleDateString('en-US', dateOptions),
+  };
+  const author = {
+    user_id: 1,
+    username: "Rene",
+    email: "rene@example.com",
+    user_avatar: "https://img.daisyui.com/images/profile/demo/2@94.webp",
+    user_country: "Germany",
+    team_name: "Zemlak, Daniel and Leannon",
+    job_name: "Desktop Support Technician",
   };
 
   return (
@@ -17,8 +27,8 @@ const News = () => {
         </figure>
         <article className="w-full h-full relative border-y-4 border-y-green-300 border-x-2">
           <header className="p-4">
-            <p className="text-md">by <strong>Admin</strong></p>
-            <p className="text-sm"><em>Nov 19, 2024</em></p>
+            <p className="text-md">by <strong>{author.username}</strong></p>
+            <p className="text-sm"><em>{news.createdAt}</em></p>
           </header>
           <section className="p-4 flex flex-wrap gap-4">
             <p>
--- src/sections/Leaderboard.jsx
+++ src/sections/Leaderboard.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 const Leaderboard = () => {
 
-  const employees = [
+  const users = [
     {
       user : {
         user_id: 1,
@@ -18,7 +18,7 @@ const Leaderboard = () => {
     {
       user: {
         user_id: 2,
-        user_name: "Biernard",
+        username: "Biernard",
         email: "biernard@example.com",
         user_avatar: "https://img.daisyui.com/images/profile/demo/3@94.webp",
         user_country: "Brazil",
@@ -30,7 +30,7 @@ const Leaderboard = () => {
     {
       user: {
         user_id: 3,
-        user_name: "Tim",
+        username: "Tim",
         email: "biernard@example.com",
         user_avatar: "https://img.daisyui.com/images/profile/demo/4@94.webp",
         user_country: "Brazil",
@@ -42,7 +42,7 @@ const Leaderboard = () => {
     {
       user: {
         user_id: 4,
-        user_name: "Choncha",
+        username: "Choncha",
         user_avatar: "https://img.daisyui.com/images/profile/demo/5@94.webp",
         user_country: "Thailand",
         team_name: "Wyman-Ledner",
@@ -53,7 +53,7 @@ const Leaderboard = () => {
     {
       user: {
         user_id: 5,
-        user_name: "Farbod",
+        username: "Farbod",
         user_avatar: "https://img.daisyui.com/images/profile/demo/1@94.webp",
         user_country: "Germany",
         team_name: "Carroll Group",
@@ -70,7 +70,6 @@ const Leaderboard = () => {
         <div className="max-w-screen-xl w-full bg-base-200 min-h-80 rounded-xl border-2 border-primary">
           <div className="overflow-x-auto">
             <table className="table bg-white">
-              {/* head */}
               <thead>
               <tr className="hover:bg-green-200">
                 <th>Name</th>
@@ -79,28 +78,27 @@ const Leaderboard = () => {
               </tr>
               </thead>
               <tbody>
-              {/* row 1 */}
-              {employees && (employees.map((employee) => (
-                <tr key={employee.user.user_id} className="hover:bg-green-200">
+              {users && (users.map((user) => (
+                <tr key={user.user.user_id} className="hover:bg-green-200">
                   <td>
                     <div className="flex items-center gap-3">
                       <div className="avatar">
                         <div className="mask mask-squircle h-12 w-12">
                           <img
-                            src={employee.user.user_avatar}
-                            alt={employee.user.username} />
+                            src={user.user.user_avatar}
+                            alt={user.user.username} />
                         </div>
                       </div>
                       <div>
-                        <Link to="#" className="font-bold cursor-pointer hover:underline">{employee.user.username}</Link>
-                        <div className="text-sm opacity-50">{employee.user.user_country}</div>
+                        <Link to="#" className="font-bold cursor-pointer hover:underline">{user.user.username}</Link>
+                        <div className="text-sm opacity-50">{user.user.user_country}</div>
                       </div>
                     </div>
                   </td>
                   <td className="hidden md:block">
-                    {employee.user.team_name} <br /> <span className="badge badge-primary badge-sm">{employee.user.job_name}</span>
+                    {user.user.team_name} <br /> <span className="badge badge-primary badge-sm">{user.user.job_name}</span>
                   </td>
-                  <td>{employee.total_points}</td>
+                  <td>{user.total_points}</td>
                 </tr>
               )))}
               </tbody>
